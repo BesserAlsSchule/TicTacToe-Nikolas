7723aa171b86165103399e9c02c2f936e19893b7
NikolasSchwarz <nikolas.schwarz95@gmail.com>
2020-10-24 21:00:08 +0200

    tictactoe

---
+++ TicTacToe1.3/TicTacToe1.3/Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.BtnSpielerEins = New System.Windows.Forms.Button()
+        Me.BtnSpielerZwei = New System.Windows.Forms.Button()
+        Me.BtnNeustart = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.BtnEnde = New System.Windows.Forms.Button()
+        Me.BtnErgebnis = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnStart
+        '
+        Me.BtnStart.Location = New System.Drawing.Point(18, 15)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(133, 51)
+        Me.BtnStart.TabIndex = 0
+        Me.BtnStart.Text = "Start"
+        Me.BtnStart.UseVisualStyleBackColor = True
+        '
+        'BtnSpielerEins
+        '
+        Me.BtnSpielerEins.Enabled = False
+        Me.BtnSpielerEins.Location = New System.Drawing.Point(837, 15)
+        Me.BtnSpielerEins.Name = "BtnSpielerEins"
+        Me.BtnSpielerEins.Size = New System.Drawing.Size(149, 51)
+        Me.BtnSpielerEins.TabIndex = 1
+        Me.BtnSpielerEins.Text = "Spieler1"
+        Me.BtnSpielerEins.UseVisualStyleBackColor = True
+        '
+        'BtnSpielerZwei
+        '
+        Me.BtnSpielerZwei.Enabled = False
+        Me.BtnSpielerZwei.Location = New System.Drawing.Point(992, 15)
+        Me.BtnSpielerZwei.Name = "BtnSpielerZwei"
+        Me.BtnSpielerZwei.Size = New System.Drawing.Size(133, 51)
+        Me.BtnSpielerZwei.TabIndex = 2
+        Me.BtnSpielerZwei.Text = "Spieler2"
+        Me.BtnSpielerZwei.UseVisualStyleBackColor = True
+        '
+        'BtnNeustart
+        '
+        Me.BtnNeustart.Enabled = False
+        Me.BtnNeustart.Location = New System.Drawing.Point(18, 496)
+        Me.BtnNeustart.Name = "BtnNeustart"
+        Me.BtnNeustart.Size = New System.Drawing.Size(133, 51)
+        Me.BtnNeustart.TabIndex = 3
+        Me.BtnNeustart.Text = "Neustart"
+        Me.BtnNeustart.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Button1.Location = New System.Drawing.Point(565, 381)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(95, 74)
+        Me.Button1.TabIndex = 4
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Button2.Location = New System.Drawing.Point(464, 383)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(95, 74)
+        Me.Button2.TabIndex = 5
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Button3.Location = New System.Drawing.Point(358, 383)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(95, 74)
+        Me.Button3.TabIndex = 6
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Button4.Location = New System.Drawing.Point(565, 301)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(95, 74)
+        Me.Button4.TabIndex = 7
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Enabled = False
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Button5.Location = New System.Drawing.Point(461, 301)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(95, 74)
+        Me.Button5.TabIndex = 8
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Enabled = False
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Button6.Location = New System.Drawing.Point(358, 301)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(95, 74)
+        Me.Button6.TabIndex = 9
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Enabled = False
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Button7.Location = New System.Drawing.Point(565, 221)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(95, 74)
+        Me.Button7.TabIndex = 10
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Enabled = False
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Button8.Location = New System.Drawing.Point(461, 220)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(95, 74)
+        Me.Button8.TabIndex = 11
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Enabled = False
+        Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Button9.Location = New System.Drawing.Point(358, 220)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(95, 74)
+        Me.Button9.TabIndex = 12
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'BtnEnde
+        '
+        Me.BtnEnde.Location = New System.Drawing.Point(992, 520)
+        Me.BtnEnde.Name = "BtnEnde"
+        Me.BtnEnde.Size = New System.Drawing.Size(133, 51)
+        Me.BtnEnde.TabIndex = 13
+        Me.BtnEnde.Text = "Ende"
+        Me.BtnEnde.UseVisualStyleBackColor = True
+        '
+        'BtnErgebnis
+        '
+        Me.BtnErgebnis.Location = New System.Drawing.Point(992, 267)
+        Me.BtnErgebnis.Name = "BtnErgebnis"
+        Me.BtnErgebnis.Size = New System.Drawing.Size(133, 51)
+        Me.BtnErgebnis.TabIndex = 14
+        Me.BtnErgebnis.Text = "Ergebnis"
+        Me.BtnErgebnis.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1137, 583)
+        Me.Controls.Add(Me.BtnErgebnis)
+        Me.Controls.Add(Me.BtnEnde)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.BtnNeustart)
+        Me.Controls.Add(Me.BtnSpielerZwei)
+        Me.Controls.Add(Me.BtnSpielerEins)
+        Me.Controls.Add(Me.BtnStart)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtnStart As Button
+    Friend WithEvents BtnSpielerEins As Button
+    Friend WithEvents BtnSpielerZwei As Button
+    Friend WithEvents BtnNeustart As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents BtnEnde As Button
+    Friend WithEvents BtnErgebnis As Button
+End Class
---
+++ TicTacToe1.3/TicTacToe1.3/Form1.vb
@@ -0,0 +1,383 @@
+﻿Public Class Form1
+    Dim Player1 As Char
+    Dim Player2 As Char
+    Dim Sieger1 As Object
+    Dim Sieger2 As Object
+
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+        BtnSpielerEins.Enabled = True
+        BtnSpielerZwei.Enabled = True
+        BtnNeustart.Enabled = True
+
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button9.Enabled = True
+
+        MessageBox.Show("Welcher Spieler Beginnt?")
+
+    End Sub
+
+    Private Sub BtnSpielerEins_Click(sender As Object, e As EventArgs) Handles BtnSpielerEins.Click
+        BtnSpielerZwei.Enabled = False
+
+        BtnStart.Enabled = False
+        Player1 = "X"
+
+    End Sub
+
+    Private Sub BtnSpielerZwei_Click(sender As Object, e As EventArgs) Handles BtnSpielerZwei.Click
+        BtnSpielerEins.Enabled = False
+
+        BtnStart.Enabled = False
+        Player2 = "O"
+
+    End Sub
+
+    Private Sub BtnNeustart_Click(sender As Object, e As EventArgs) Handles BtnNeustart.Click
+
+        Button1.Text = " "
+        Button2.Text = " "
+        Button3.Text = " "
+        Button4.Text = " "
+        Button5.Text = " "
+        Button6.Text = " "
+        Button7.Text = " "
+        Button8.Text = " "
+        Button9.Text = " "
+
+        BtnStart.Enabled = True
+
+        BtnSpielerEins.Enabled = False
+        BtnSpielerZwei.Enabled = False
+
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button1.TextChanged
+
+
+        If BtnSpielerEins.Enabled = True Then
+
+            Button1.Text = Player1
+
+        Else
+            Button1.Text = Player2
+
+        End If
+
+        If Button1.Text = "X" Then
+            BtnSpielerEins.Enabled = False
+            BtnSpielerZwei.Enabled = True
+
+        Else
+            If Button1.Text = "O" Then
+                BtnSpielerZwei.Enabled = False
+                BtnSpielerEins.Enabled = True
+            End If
+
+        End If
+
+        Button1.Enabled = False
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If BtnSpielerEins.Enabled = True Then
+
+            Button2.Text = Player1
+
+        Else
+            Button2.Text = Player2
+
+        End If
+
+        If Button2.Text = "X" Then
+            BtnSpielerEins.Enabled = False
+            BtnSpielerZwei.Enabled = True
+
+        Else
+            If Button2.Text = "O" Then
+                BtnSpielerZwei.Enabled = False
+                BtnSpielerEins.Enabled = True
+            End If
+
+        End If
+
+        Button2.Enabled = False
+
+    End Sub
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If BtnSpielerEins.Enabled = True Then
+
+            Button3.Text = Player1
+
+        Else
+            Button3.Text = Player2
+
+        End If
+
+        If Button3.Text = "X" Then
+            BtnSpielerEins.Enabled = False
+            BtnSpielerZwei.Enabled = True
+
+        Else
+            If Button3.Text = "O" Then
+                BtnSpielerZwei.Enabled = False
+                BtnSpielerEins.Enabled = True
+            End If
+
+        End If
+
+        Button3.Enabled = False
+
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If BtnSpielerEins.Enabled = True Then
+
+            Button4.Text = Player1
+
+        Else
+            Button4.Text = Player2
+
+        End If
+
+        If Button4.Text = "X" Then
+            BtnSpielerEins.Enabled = False
+            BtnSpielerZwei.Enabled = True
+
+        Else
+            If Button4.Text = "O" Then
+                BtnSpielerZwei.Enabled = False
+                BtnSpielerEins.Enabled = True
+            End If
+
+        End If
+
+        Button4.Enabled = False
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If BtnSpielerEins.Enabled = True Then
+
+            Button5.Text = Player1
+
+        Else
+            Button5.Text = Player2
+
+        End If
+
+        If Button5.Text = "X" Then
+            BtnSpielerEins.Enabled = False
+            BtnSpielerZwei.Enabled = True
+
+        Else
+            If Button5.Text = "O" Then
+                BtnSpielerZwei.Enabled = False
+                BtnSpielerEins.Enabled = True
+            End If
+
+        End If
+
+        Button5.Enabled = False
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If BtnSpielerEins.Enabled = True Then
+
+            Button6.Text = Player1
+
+        Else
+            Button6.Text = Player2
+
+        End If
+
+        If Button6.Text = "X" Then
+            BtnSpielerEins.Enabled = False
+            BtnSpielerZwei.Enabled = True
+
+        Else
+            If Button6.Text = "O" Then
+                BtnSpielerZwei.Enabled = False
+                BtnSpielerEins.Enabled = True
+            End If
+
+        End If
+
+        Button6.Enabled = False
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If BtnSpielerEins.Enabled = True Then
+
+            Button7.Text = Player1
+
+        Else
+            Button7.Text = Player2
+
+        End If
+
+        If Button7.Text = "X" Then
+            BtnSpielerEins.Enabled = False
+            BtnSpielerZwei.Enabled = True
+
+        Else
+            If Button7.Text = "O" Then
+                BtnSpielerZwei.Enabled = False
+                BtnSpielerEins.Enabled = True
+            End If
+
+        End If
+
+        Button7.Enabled = False
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If BtnSpielerEins.Enabled = True Then
+
+            Button8.Text = Player1
+
+        Else
+            Button8.Text = Player2
+
+        End If
+
+        If Button8.Text = "X" Then
+            BtnSpielerEins.Enabled = False
+            BtnSpielerZwei.Enabled = True
+
+        Else
+            If Button8.Text = "O" Then
+                BtnSpielerZwei.Enabled = False
+                BtnSpielerEins.Enabled = True
+            End If
+
+        End If
+
+        Button8.Enabled = False
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If BtnSpielerEins.Enabled = True Then
+
+            Button9.Text = Player1
+
+        Else
+            Button9.Text = Player2
+
+        End If
+
+        If Button9.Text = "X" Then
+            BtnSpielerEins.Enabled = False
+            BtnSpielerZwei.Enabled = True
+
+        Else
+            If Button9.Text = "O" Then
+                BtnSpielerZwei.Enabled = False
+                BtnSpielerEins.Enabled = True
+            End If
+
+        End If
+
+        Button9.Enabled = False
+
+    End Sub
+
+    Private Sub BtnEnde_Click(sender As Object, e As EventArgs) Handles BtnEnde.Click
+        MessageBox.Show("Das Programm wird beendet")
+
+        Console.ReadLine()
+
+        End
+
+    End Sub
+
+    Private Sub BtnErgebnis_Click(sender As Object, e As EventArgs) Handles BtnErgebnis.Click
+
+
+
+        If Button1.Text = "X" AndAlso Button2.Text = "X" AndAlso Button3.Text = "X" Then
+            MessageBox.Show("Spieler1 gewinnt!")
+
+
+        ElseIf Button1.Text = "O" AndAlso Button2.Text = "O" AndAlso Button3.Text = "O" Then
+            MessageBox.Show("Spieler2 gewinnt!")
+
+
+        ElseIf Button1.Text = "X" AndAlso Button4.Text = "X" AndAlso Button7.Text = "X" Then
+            MessageBox.Show("Spieler1 gewinnt!")
+
+        ElseIf Button1.Text = "O" AndAlso Button4.Text = "O" AndAlso Button7.Text = "O" Then
+            MessageBox.Show("Spieler2 gewinnt!")
+
+        ElseIf Button4.Text = "X" AndAlso Button5.Text = "X" AndAlso Button6.Text = "X" Then
+            MessageBox.Show("Spieler1 gewinnt!")
+
+        ElseIf Button4.Text = "O" AndAlso Button5.Text = "O" AndAlso Button6.Text = "O" Then
+            MessageBox.Show("Spieler2 gewinnt!")
+
+        ElseIf Button3.Text = "X" AndAlso Button6.Text = "X" AndAlso Button9.Text = "X" Then
+            MessageBox.Show("Spieler1 gewinnt!")
+
+        ElseIf Button3.Text = "O" AndAlso Button6.Text = "O" AndAlso Button9.Text = "O" Then
+            MessageBox.Show("Spieler2 gewinnt!")
+
+        ElseIf Button7.Text = "X" AndAlso Button8.Text = "X" AndAlso Button9.Text = "X" Then
+            MessageBox.Show("Spieler1 gewinnt!")
+
+        ElseIf Button7.Text = "O" AndAlso Button8.Text = "O" AndAlso Button9.Text = "O" Then
+            MessageBox.Show("Spieler2 gewinnt!")
+
+        ElseIf Button2.Text = "X" AndAlso Button5.Text = "X" AndAlso Button8.Text = "X" Then
+            MessageBox.Show("Spieler1 gewinnt!")
+
+        ElseIf Button2.Text = "O" AndAlso Button5.Text = "O" AndAlso Button8.Text = "O" Then
+            MessageBox.Show("Spieler2 gewinnt!")
+
+        ElseIf Button1.Text = "X" AndAlso Button5.Text = "X" AndAlso Button9.Text = "X" Then
+            MessageBox.Show("Spieler1 gewinnt!")
+
+        ElseIf Button1.Text = "O" AndAlso Button5.Text = "O" AndAlso Button9.Text = "O" Then
+            MessageBox.Show("Spieler2 gewinnt!")
+
+        ElseIf Button3.Text = "X" AndAlso Button5.Text = "X" AndAlso Button7.Text = "X" Then
+            MessageBox.Show("Spieler1 gewinnt!")
+
+        ElseIf Button3.Text = "O" AndAlso Button5.Text = "O" AndAlso Button7.Text = "O" Then
+            MessageBox.Show("Spieler2 gewinnt!")
+
+        Else
+            MessageBox.Show("Unentschieden!")
+        End If
+
+
+    End Sub
+End Class
+
